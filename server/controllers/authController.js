ea7bb129f2e35102b423b4a9b068bfbe1c939ede
MikiZenebe <mickyaszenebe@gmail.com>
2024-06-06 22:25:13 -0500

    auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -48,5 +48,37 @@ export const signup = async (req, res) => {
     } else {
       res.status(400).json({ error: "Invalid user data" });
     }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPassCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if (!user || !isPassCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    genTokenAndSetCookie(user?._id, res);
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
