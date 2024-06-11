06a6974d90dcc7bef4fbfc922826b170e1ac8f7d
MikiZenebe <mickyaszenebe@gmail.com>
2024-06-11 12:22:07 -0500

    auth-ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Login from "./pages/auth/LoginPage";
+import Signup from "./pages/auth/SignUpPage";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className="flex max-w-6xl mx-auto ">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 }
