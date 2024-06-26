7348663a3ffa7622bdcf91f3fa429df663f1e66d
MikiZenebe <mickyaszenebe@gmail.com>
2024-06-26 09:53:36 -0500

    fixed

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -10,6 +11,8 @@ import notificationRoutes from "./routes/notificationRoutes.js";
 
 //Initial
 const app = express();
+const __dirname = path.resolve();
+
 dotenv.config();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -19,13 +22,22 @@ cloudinary.config({
 
 //Middileware
 app.use(cookieParser());
-app.use(express.json()); //to parse req.body
+app.use(express.json({ limit: "50mb" })); // to parse req.body
+// limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/notification", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(5000, () => {
   console.log("Server is running");
